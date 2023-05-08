ae2a8c3a3614117d5425df86b68a1f908675b2e8
Iulik47 <iulikk2013@gmail.com>
2023-05-08 20:13:10 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/org/Iulik/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,39 @@
+package org.Iulik.variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+// Initializarea variabilelor declarate
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'Z';
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfByteDataType = -128;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+
+
+        System.out.println(firstCharFromAscii);
+
+    }
+
+
+}
